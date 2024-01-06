6aafe27c4638870222e2aa215e7fabd8c100534f
Alishbaprograms <ash.k1740@gmail.com>
2024-01-06 19:44:13 +0500

    background

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 864
+SCREEN_HEIGHT = 936
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('Alishba- Flappy Bird')
 #player=pygame.Rect(x,y,width,height)
 #load images
-bg=pygame.image.load('img/bg.png')
+bg=pygame.image.load('Flappy-Bird---Pygame/img/bg.png')
+ground=pygame.image.load('Flappy-Bird---Pygame/img/ground.png')
 run = True
 while run:
     screen.blit(bg,(0,0))
-    
+    screen.blit(ground,(0,768))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
