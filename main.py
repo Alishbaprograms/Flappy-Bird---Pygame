0c20c02b2067f5f9c0881bc899dce735eabe7782
Alishbaprograms <ash.k1740@gmail.com>
2024-01-06 19:35:38 +0500

    initial commit

--- main.py
+++ main.py
@@ -8,22 +8,12 @@ SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('Alishba- Flappy Bird')
 #player=pygame.Rect(x,y,width,height)
-player = pygame.Rect(300,250,50,50)
+#load images
+bg=pygame.image.load('img/bg.png')
 run = True
 while run:
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen,(255,0,0),player)
-
-    key=pygame.key.get_pressed()
-    if key[pygame.K_a]==True:
-        player.move_ip(-1,0)
-    elif key[pygame.K_d]==True:
-        player.move_ip(1,0)
-    elif key[pygame.K_w]==True:
-        player.move_ip(0,-1)
-    elif key[pygame.K_s]==True:
-        player.move_ip(0,1)
-
+    screen.blit(bg,(0,0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
